f293899a510ad00989a4c21a304f7505e190d1de
AlcidesC.Chitsumba <alcidesdaconceicao03@gmail.com>
2024-10-03 04:17:28 +0200

    !

---
+++ src/Services.jsx
@@ -0,0 +1,45 @@
+import NavBar from "./NavBar/NavBar";
+import Hero from "./Home/Hero";
+import Discover from "./Home/Discover";
+import About from "./Home/About";
+import Form from "./Home/Form";
+import Footer from "./NavBar/Footer";
+import bgImg_1 from "./assets/bg-1.png";
+
+
+function Home() {
+  return (
+    <>
+      <div>
+        <main style={{'--image-url': `url(${bgImg_1})`}} className='bg-[image:var(--image-url)] bg-fixed'>
+          <nav>
+            {" "}
+            <NavBar />{" "}
+          </nav>
+
+          <header>
+          </header>
+
+          {/*Discover section*/}
+          <section>
+            <Discover />
+          </section>
+
+          {/*About section*/}
+          <section>
+          </section>
+
+          {/*Form section*/}
+          <section>
+          </section>
+
+          <footer className="bg-gray-900">
+            <Footer/>
+          </footer>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default Home;
